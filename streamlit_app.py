a0fa60bc5ace58ead39ef6e2210c353ccf773346
ganesanji <137085523+ganesanji@users.noreply.github.com>
2023-06-21 21:37:03 +0530

    Update streamlit_app.py
    removed test lines and set index for data frame list

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#df = pandas.DataFrame(my_fruit_list)
-streamlit.text("From pandas data frame")
-#print(df)
