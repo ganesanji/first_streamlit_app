47e4f69a61fe94dfcc0613accdc0c320147df983
ganesanji <137085523+ganesanji@users.noreply.github.com>
2023-06-21 21:31:29 +0530

    Update streamlit_app.py
    using pandas data frame method

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+df = pandas.DataFrame(my_fruit_list)
+print("From pandas data frame")
+print(df)
